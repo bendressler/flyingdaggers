2a49ffa0c4a8ec3c0dec02c83f5fb3584f2c5629
bendressler <bj.dressler@gmail.com>
2017-01-14 21:25:17 +0100

    first

---
+++ daggers_prototype/Assets/Scripts/ClickMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickMove : MonoBehaviour {
+
+	public GameObject marker;
+	public GameObject hero;
+	public int maxwpnts;
+	public ArrayList waypoints = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetMouseButtonUp (0)) {
+			if (Physics.Raycast (ray, out hit, 100)) {
+				if ((hit.collider.CompareTag ("terrain")) && (!hit.collider.CompareTag ("barrier"))) {
+					Debug.Log ("Creating waypoint");
+					GameObject waypoint = (GameObject) Instantiate (marker, hit.point, Quaternion.identity);
+					hero.GetComponent<HeroMove>().nextwaypoint = waypoint;
+					waypoints.Add (waypoint);
+					Debug.Log(waypoints.Count);
+				}
+			}
+		}
+	}
+}
---
+++ daggers_prototype/Assets/Scripts/VillainClass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillainClass : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			GetComponent<NavMeshAgent> ().destination = target.transform.position;
+		}
+		transform.eulerAngles = new Vector3(0,0,0);
+	}
+}
